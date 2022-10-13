d3f00e9066ee162a103047ea7a0f2f9793817d79
abelzer09 <aaronbelzer09@gmail.com>
2022-10-13 15:41:04 -0500

    created html layout for calculator in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 
 
 function App() {
-  return ;
+  return (
+    <div className="project-grid">
+      <div className="display">
+        <div className="prev-op"></div>
+        <div className="cur-op"></div>
+      </div>
+      <button className="size-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="size-two">=</button>
+    </div>
+  )
 }
 
 export default App;
